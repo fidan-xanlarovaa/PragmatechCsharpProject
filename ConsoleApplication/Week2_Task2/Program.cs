249e2515c811bf738f5a34e357207588064583c0
xanlarova.fidan <xanlarovaa.fidan@gmail.com>
2021-06-18 23:36:30 +0400

    Create new solution for tasks and copy all tasks again

---
+++ ConsoleApplication/Week2_Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Week2_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer1, integer2;
+
+            Console.WriteLine("Pls enter first integer");
+            integer1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pls enter second integer");
+            integer2 = Convert.ToInt32(Console.ReadLine());
+
+            if (integer1 > integer2)
+            {
+                Console.WriteLine($"First integer {integer1} is greater than the second intger {integer2}.");
+            }
+            else if (integer1 < integer2)
+            {
+                Console.WriteLine($"Second integer {integer2} is greater than the first intger {integer1}.");
+            }
+            else
+            {
+                Console.WriteLine($"First integer {integer1} and the second intger {integer2} are equal.");
+            }
+        }
+    }
+}
---
+++ ConsoleApps/Week1/Week1_Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week1_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
