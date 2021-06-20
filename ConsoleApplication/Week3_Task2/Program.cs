73cd16e25c9eab708992462b0bd5c14f6c7a3aac
xanlarova.fidan <xanlarovaa.fidan@gmail.com>
2021-06-20 13:38:26 +0400

    Week3_Task2 done

---
+++ ConsoleApplication/Week3_Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Week3_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number,sum=0;
+
+            do
+            {
+                Console.WriteLine("Pls enter a number");
+                number =Convert.ToInt32(Console.ReadLine());
+                sum += number;
+            } while (number != 0);
+
+            Console.WriteLine($"The sum of the entered numbers equal to the {sum}");
+
+           
+        }
+    }
+}
