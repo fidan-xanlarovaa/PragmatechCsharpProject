110decd8246c07a9551d4bc9b1de8b37f5fc27bb
xanlarova.fidan <xanlarovaa.fidan@gmail.com>
2021-06-21 10:30:16 +0400

    Week3_Task11 and Week3_Task12 done

--- ConsoleApplication/Week3_Task11/Program.cs
+++ ConsoleApplication/Week3_Task11/Program.cs
@@ -6,7 +6,24 @@ namespace Week3_Task11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Prime numbers till 100");
+            for (int i = 2; i < 101; i++)
+            {
+                int k = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        k++;
+                    }
+                }
+
+                if (k == 2)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
         }
     }
 }
--- ConsoleApplication/Week3_Task12/Program.cs
+++ ConsoleApplication/Week3_Task12/Program.cs
@@ -6,7 +6,29 @@ namespace Week3_Task12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Pls enter 2 numbers.");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Prime numbers betwen {number1} and {number2}.");
+            for (int i = number1; i <= number2; i++)
+            {
+                int k = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        k++;
+                    }
+                }
+
+                if (k == 2)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
         }
     }
-}
+    }
+
