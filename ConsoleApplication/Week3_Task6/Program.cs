2d62c42e5f1f5a482dcb15d97933c9e7d2db6b54
xanlarova.fidan <xanlarovaa.fidan@gmail.com>
2021-06-25 16:36:10 +0400

    Week3_Task6 and Week3_Task8 problems solved

--- ConsoleApplication/Week3_Task6/Program.cs
+++ ConsoleApplication/Week3_Task6/Program.cs
@@ -4,19 +4,29 @@ namespace Week3_Task6
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
             int number;
+            
             Console.WriteLine("How many names would you insert?");
             number = Convert.ToInt32(Console.ReadLine());
-            
+
+            int[] RandomNumbers = new int[number];
+            Random RandomNum = new Random();
+           
+            string[] Names = new string[number];
+            string[] firstCommand = new string[number/2];
+            string[] secondCommand = new string[number/2];
+
             while (number % 2 != 0)
             {
                 Console.WriteLine("Pls enter an even number");
                 number = Convert.ToInt32(Console.ReadLine());
             }
 
-            string[] Names = new string[number];
+            
 
             for(int i = 0; i < number; i++)
             {
@@ -24,14 +34,15 @@ namespace Week3_Task6
                 Names[i] = Console.ReadLine();
 
                 if (i > 0) { 
-                
+                    x:
                     for (int j = i-1; j >= 0; j--) {
                     
                         
-                        while (Names[j] == Names[i]) 
+                        if (Names[j] == Names[i]) 
                         { 
                             Console.WriteLine("This name alredy entered pls enter another name,this name alredy entered");
                             Names[i] = Console.ReadLine();
+                            goto x;
                         }
                         
                
@@ -48,40 +59,48 @@ namespace Week3_Task6
                 Console.WriteLine(Names[i]);
             }
 
+    
+          for (int i = 0; i < number; i++)
+          {
+              RandomNumbers[i] = RandomNum.Next(number);
 
-            /*
+              if (i > 0)
+              {
+                  x:
+                  for (int j = i - 1; j >= 0; j--)
+                  {
+                      if (RandomNumbers[j] == RandomNumbers[i])
+                      {
+                          RandomNumbers[i] = RandomNum.Next(number);
+                          goto x;
 
-                        int[] RandomNumbers = new int[number];
-
-                        for (int i = 0; i < number; i++)
-                        {
-                            Random RandomNum = new Random();
-                            int random = RandomNum.Next(number);
-
-                            RandomNumbers[i] = random;
-
-                            if (i > 0)
-                            {
-
-                                for (int j = i - 1; j >= 0; j--)
-                                {
-
-
-                                    while (RandomNumbers[j] == RandomNumbers[i])
-                                    {
-
-                                         random = RandomNum.Next(number);
-
-                                        RandomNumbers[i] = random;
-                                    }
+                      }
+                  }
+              }
 
 
-                                }
-                            }
-                        }*/
+          }
+
+
+          for (int i = 0; i < number/2; i++)
+          {
+                firstCommand[i] = Names[RandomNumbers[i]];
+                secondCommand[i] = Names[RandomNumbers[number - i-1]];
+
+          }
+
+            Console.WriteLine("\n\nThese are our first team members\tThese are our second team members");
+            for (int i = 0; i < number / 2; i++)
+            {
+                Console.WriteLine($"{firstCommand[i]}\t\t\t\t\t{secondCommand[i]}");
+            }
 
 
 
         }
+
+
+
     }
-}
+    }
+
