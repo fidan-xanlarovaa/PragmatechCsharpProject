d0f42e854e31c353f2d771ba89c7b8dc594e9339
xanlarova.fidan <xanlarovaa.fidan@gmail.com>
2021-06-25 16:39:39 +0400

    Week3_Task4 first solution method again pushed

---
+++ ConsoleApplication/Week3_Task4_firstsolution/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Week3_Task4_firstsolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            do
+            {
+                Console.WriteLine("Pls enter a number");
+                number = Convert.ToInt32(Console.ReadLine());
+
+            } while (number == 0);
+
+            int factorial = number;
+            for (int i = number - 1; i >= 2; i--)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine($"Factorial of {number} is equal to {factorial}");
+        }
+    }
+}
