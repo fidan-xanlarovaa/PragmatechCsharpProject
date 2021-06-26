3040e703af0f240ff6d3b8e51ec1863161a8a2ba
xanlarova.fidan <xanlarovaa.fidan@gmail.com>
2021-06-27 00:42:00 +0400

    Week3_Task8 issue solved

---
+++ ConsoleApplications/Week3/Week3_Task7_second_method/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Week3_Task7_second_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
---
+++ ConsoleApplications/Week3/Week3_Task8_second_method/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Week3_Task8_second_method
+{
+    class Program
+    {
+        public int i;
+
+        static void Main(string[] args)
+        {
+            int total = 0;
+            int[] RandomNumbers = new int[10];
+            Random RandomNum = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                RandomNumbers[i] = RandomNum.Next(10);
+
+            x:
+                
+
+                if (i > 0)
+                {
+
+                    for (int j = i - 1; j >= 0; j--)
+                    {
+                        if (RandomNumbers[j] == RandomNumbers[i])
+                        {
+                            RandomNumbers[i] = RandomNum.Next(10);
+                            total++;
+                            goto x;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("\n\nOur array elements:\n");
+
+            string str = String.Join(",", RandomNumbers);
+
+            Console.WriteLine(str);
+
+
+            Console.WriteLine($"\n\nThe total numberof duplicates displayed is {total}.\n");
+
+            
+
+        }
+    }
+}
