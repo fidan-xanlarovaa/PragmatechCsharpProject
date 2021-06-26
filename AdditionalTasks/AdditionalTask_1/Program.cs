c1d5a3e41991fd020bba05d1207c2b071e28b83e
xanlarova.fidan <xanlarovaa.fidan@gmail.com>
2021-06-27 01:05:23 +0400

    AdditionalTask_1 done

---
+++ AdditionalTasks/AdditionalTask_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AdditionalTask_1
+{
+    /*Write a C# Sharp program which takes a positive number and return the nth odd number.Sample Output:
+      1st odd number: 1
+      2nd odd number: 3
+      4th odd number: 7
+      100th odd number: 199*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pls enter a positive number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{number}th odd number is {number * 2 - 1}");
+        }
+    }
+}
